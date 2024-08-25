044ac798971dbcab03b14b0b5faa3bc3db126ea5
Shan <sskset@gmail.com>
2024-08-25 23:39:08 +1000

    add project

---
+++ CodeDance.Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeDance.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source == null || !source.Any();
+        }
+
+        public static bool NotNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Any();
+        }
+
+        public static IEnumerable<T> OrEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source.IsNullOrEmpty())
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            return source;
+        }
+    }
+}
