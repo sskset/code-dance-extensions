1f4e0b1ba31a1ec682a141281fd0f2b30910e53f
Shan <sskset@gmail.com>
2024-09-12 17:34:54 +1000

    add IsEmail

---
+++ CodeDance.Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeDance.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmail(this string value)
+        {
+            // Regular expression pattern to match a valid email format
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(value, pattern);
+        }
+    }
+}
